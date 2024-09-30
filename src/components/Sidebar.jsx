8a035e44c9e342003842986f5f80c70c30825a9a
Java-rice <johnmark.p.peroche@gmail.com>
2024-09-30 20:00:21 +0800

    style: adjust components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = ({ isExpanded }) => {
     <div
       className={`sidebar font-montserrat p-4 bg-[#F2EAE1] min-h-screen transition-all duration-300 
         ${isExpanded ? 'w-1/5' : 'w-20'} 
-        md:${isExpanded ? 'w-1/5' : 'w-16'}`} 
+        md:${isExpanded ? 'w-1/5' : 'w-16'}`} // Adjust the width at medium screens
     >
       <h2 className={`border-l-4 px-4 border-[#FEAF00] font-bold text-lg ${isExpanded ? 'block' : 'hidden'}`}>
         CRUD OPERATIONS
@@ -41,7 +41,7 @@ const Sidebar = ({ isExpanded }) => {
                   : 'flex items-center text-black text-sm hover:bg-[#FEAF00] hover:rounded-md px-4 py-2 transition'
               }
             >
-              <img src={icon} alt={label} className={`transition-all duration-300 ${isExpanded ? 'h-4 ml-16' : 'w-4 h-4'} mr-2`} />
+              <img src={icon} alt={label} className={`transition-all duration-300 ${isExpanded ? 'h-4' : 'w-4 h-4'} mr-2`} />
               {/* Show label only if expanded or if screen size is small */}
               <span className={`transition-all duration-300 ${isExpanded || window.innerWidth < 768 ? '' : 'hidden'}`}>
                 {label}
@@ -53,7 +53,7 @@ const Sidebar = ({ isExpanded }) => {
 
       {/* Logout Button */}
       <div className={`mt-20 ${isExpanded ? 'px-4' : 'px-0' }`}>
-        <button className={`w-full flex items-center justify-center text-black text-sm rounded-md p-2 hover:bg-[#FEAF00] transition`}>
+        <button className={`w-full flex items-center ${isExpanded ? 'justify-center' : 'justify-start' } text-black text-sm rounded-md p-2 hover:bg-[#FEAF00] transition`}>
           {isExpanded && 'Logout'}
           <img src={signout} alt="sign out" className={`transition-all duration-300 ${isExpanded ? 'w-4 ml-2' : 'w-4 h-4 ml-0 mr-2'}`} />
         </button>
